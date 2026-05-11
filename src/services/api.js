d8b9dc93f68a71944b3f1ba452bcc6cf2d971174
GiovannyPadilla <giovanny.padilla@udla.edu.ec>
2026-05-11 02:23:20 -0500

    cambio url conexion

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://corebackendgp.onrender.com',
+    baseURL: 'https://corebackendgp.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
